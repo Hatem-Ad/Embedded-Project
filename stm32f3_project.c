d2a657e172c9672d06ec7129b5f05f02ff901053
Hatem-Ad <45299723+Hatem-Ad@users.noreply.github.com>
2024-06-08 12:22:19 +0100

    Update stm32f3_project.c

--- stm32f3_project.c
+++ stm32f3_project.c
@@ -109,6 +109,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  // just led 
 	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, GPIO_PIN_SET);
 	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12
                               |GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_RESET);
