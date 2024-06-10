de47b60b33a0d413ca7201f9e6e960a5fb81358f
Hatem-Ad <45299723+Hatem-Ad@users.noreply.github.com>
2024-06-10 23:37:36 +0100

    Update stm32f3_project.c

--- stm32f3_project.c
+++ stm32f3_project.c
@@ -1,7 +1,6 @@
 // we will start with stm32f3 blink led
 // the objective is write a structer for boot and driver
 // Build a solid structer to STM32 firmware
-// Write without HAL layer
 
 /* USER CODE BEGIN Header */
 /**
